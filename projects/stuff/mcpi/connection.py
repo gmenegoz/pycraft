49f6331ee95e3a8c807d74d313ef3adce006ce1b
Alessandro Norfo <sprintingkiwi@gmail.com>
2016-10-25 10:03:51 +0200

    maze working, turtle started conversion, util file used again

--- projects/stuff/mcpi/connection.py
+++ projects/stuff/mcpi/connection.py
@@ -1,20 +1,10 @@
 import socket
 import select
 import sys
-import collections
+from util import *
 
 """ @author: Aron Nieminen, Mojang AB"""
 
-
-def flatten(l):
-    for e in l:
-        if isinstance(e, collections.Iterable) and not isinstance(e, basestring):
-            for ee in flatten(e): yield ee
-        else: yield e
-
-
-def flatten_parameters_to_string(l):
-    return ",".join(map(str, flatten(l)))
 
 
 class RequestError(Exception):
--- projects/stuff/mcpi/minecraftturtle.py
+++ projects/stuff/mcpi/minecraftturtle.py
@@ -2,31 +2,21 @@
 #Martin O'Hanlon
 #www.stuffaboutcode.com
 
-import minecraft
-import block
 import time
 import math
-import collections
-
-
-def flatten(l):
-    for e in l:
-        if isinstance(e, collections.Iterable) and not isinstance(e, basestring):
-            for ee in flatten(e): yield ee
-        else: yield e
-
-
-def flatten_parameters_to_string(l):
-    return ",".join(map(str, flatten(l)))
+import blockslist as bl
+from vec3 import Vec3
+from util import *
 
 
 class MinecraftTurtle:
 
     SPEEDTIMES = {0:0, 12:0.001, 11:0.01, 10:0.1, 9:0.2, 8:0.3, 7:0.4, 6:0.5, 5:0.6, 4:0.7, 3:0.8, 2:0.9, 1:1}
 
-    def __init__(self, mc, mcdrawing, position, player):
+    def __init__(self, conn, position, player):
         #set defaults
-        self.mc = mc
+        self.conn = conn
+        self.target = player
         self.player = player
         #start position
         self.startposition = position
@@ -37,24 +27,22 @@ class MinecraftTurtle:
         self.verticalheading = 0
         #set pen down
         self._pendown = True
-        #set pen block to black wool
-        self._penblock = block.Block(block.WOOL.id, 15)
+        #set pen bl to black wool
+        self._penbl = bl.Block(bl.WOOL.id, 15)
         #flying to true
         self.flying = True
         #set speed
         self.turtlespeed = 6
         #create turtle
         self.showturtle = True
-        # create drawing object
-        self.mcDrawing = mcdrawing
-        # set turtle block
-        self.turtleblock = block.Block(block.DIAMOND_BLOCK.id)
+        # set turtle bl
+        self.turtlebl = bl.Block(bl.DIAMOND_BLOCK.id)
         # draw turtle
         self._drawTurtle(int(self.position.x), int(self.position.y), int(self.position.y))
         #previous vertical heading
         self.previous = 0
         #last turtle
-        self.lastDrawnTurtle = minecraft.Vec3(0,0,0)
+        self.lastDrawnTurtle = Vec3(0,0,0)
 
     def forward(self, distance):
         #get end of line
@@ -72,10 +60,10 @@ class MinecraftTurtle:
         self._moveTurtle(x,y,z)
 
     def _moveTurtle(self,x,y,z):
-        #get blocks between current position and next
+        #get bls between current position and next
         targetX, targetY, targetZ = int(x), int(y), int(z)
         #if walking, set target Y to be height of world
-        if self.flying == False: targetY = self.mc.getHeight(targetX, targetZ)
+        if self.flying == False: targetY = int(self.conn.sendReceive("world.getHeight", intFloor(targetX, targetZ)))
         currentX, currentY, currentZ = int(self.position.x), int(self.position.y), int(self.position.z)
 
 
@@ -85,72 +73,72 @@ class MinecraftTurtle:
         #if speed is 0 and flying, just draw the line, else animate it
         if self.turtlespeed == 0 and self.flying:
             #draw the line
-            if self._pendown: self.mcDrawing.drawLine(currentX, currentY - 1, currentZ, targetX, targetY - 1, targetZ, self._penblock.id, self._penblock.data)
+            if self._pendown: self.mcDrawing.drawLine(currentX, currentY - 1, currentZ, targetX, targetY - 1, targetZ, self._penbl.id, self._penbl.data)
         else:
-            blocksBetween = self.mcDrawing.getLine(currentX, currentY, currentZ, targetX, targetY, targetZ)
+            blsBetween = self.mcDrawing.getLine(currentX, currentY, currentZ, targetX, targetY, targetZ)
             if self.verticalheading > 215 and self.verticalheading < 315:
                 self.previous = -1
-                for blockBetween in blocksBetween:
+                for blBetween in blsBetween:
                     #if walking update the y, to be the height of the world
-                    if self.flying == False: blockBetween.y = self.mc.getHeight(blockBetween.x, blockBetween.z)
+                    if self.flying == False: blBetween.y = int(self.conn.sendReceive("world.getHeight", intFloor(blBetween.x, blBetween.z)))
                     #check the material on the new turtle position
-                    #replace = self.mc.getBlock(blockBetween.x, blockBetween.y, blockBetween.z)
+                    #replace = self.mc.getBlock(blBetween.x, blBetween.y, blBetween.z)
                     #draw the turtle
-                    if self.showturtle: self._drawTurtle(blockBetween.x, blockBetween.y-2, blockBetween.z)
+                    if self.showturtle: self._drawTurtle(blBetween.x, blBetween.y-2, blBetween.z)
                     #draw the pen
-                    if self._pendown: self.mcDrawing.drawPoint3d(blockBetween.x, blockBetween.y - 1, blockBetween.z, self._penblock.id, self._penblock.data)
+                    if self._pendown: self.conn.send("world.setBlock", intFloor(blBetween.x, blBetween.y - 1, blBetween.z, self._penbl.id, self._penbl.data))
                     #wait
                     time.sleep(self.SPEEDTIMES[self.turtlespeed])
                     #clear the turtle
-                    if self.showturtle: self._clearTurtle(blockBetween.x, blockBetween.y-2, blockBetween.z)
+                    if self.showturtle: self._clearTurtle(blBetween.x, blBetween.y-2, blBetween.z)
                 #update turtle's position to be the target
                 self.position.x, self.position.y, self.position.z = x,y,z
                 #draw turtle
                 if self.showturtle: self._drawTurtle(targetX, targetY - 2, targetZ)
             elif self.verticalheading > 45 and self.verticalheading < 135:
                 self.previous = 1
-                for blockBetween in blocksBetween:
-                    #print blockBetween
+                for blBetween in blsBetween:
+                    #print blBetween
                     #if walking update the y, to be the height of the world
-                    if self.flying == False: blockBetween.y = self.mc.getHeight(blockBetween.x, blockBetween.z)
+                    if self.flying == False: blBetween.y = int(self.conn.sendReceive("world.getHeight", intFloor(blBetween.x, blBetween.z)))
                     #draw the turtle
-                    if self.showturtle: self._drawTurtle(blockBetween.x, blockBetween.y, blockBetween.z)
+                    if self.showturtle: self._drawTurtle(blBetween.x, blBetween.y, blBetween.z)
                     #draw the pen
-                    if self._pendown: self.mcDrawing.drawPoint3d(blockBetween.x, blockBetween.y - 1, blockBetween.z, self._penblock.id, self._penblock.data)
+                    if self._pendown: self.conn.send("world.setBlock", intFloor(blBetween.x, blBetween.y - 1, blBetween.z, self._penbl.id, self._penbl.data))
                     #wait
                     time.sleep(self.SPEEDTIMES[self.turtlespeed])
                     #clear the turtle
-                    if self.showturtle: self._clearTurtle(blockBetween.x, blockBetween.y, blockBetween.z)
+                    if self.showturtle: self._clearTurtle(blBetween.x, blBetween.y, blBetween.z)
                 #update turtle's position to be the target
                 self.position.x, self.position.y, self.position.z = x,y,z
                 #draw turtle
                 if self.showturtle: self._drawTurtle(targetX, targetY, targetZ)
             else:
                 if self.previous == -1:
-                    for blockBetween in blocksBetween:
-                        #print blockBetween
+                    for blBetween in blsBetween:
+                        #print blBetween
                         #if walking update the y, to be the height of the world
-                        if self.flying == False: blockBetween.y = self.mc.getHeight(blockBetween.x, blockBetween.z)
+                        if self.flying == False: blBetween.y = int(self.conn.sendReceive("world.getHeight", intFloor(blBetween.x, blBetween.z)))
                         #draw the turtle
-                        if self.showturtle: self._drawTurtle(blockBetween.x, blockBetween.y-2, blockBetween.z)
-                        if self._pendown: self.mcDrawing.drawPoint3d(blockBetween.x, blockBetween.y - 1, blockBetween.z, self._penblock.id, self._penblock.data)
+                        if self.showturtle: self._drawTurtle(blBetween.x, blBetween.y-2, blBetween.z)
+                        if self._pendown: self.conn.send("world.setBlock", intFloor(blBetween.x, blBetween.y - 1, blBetween.z, self._penbl.id, self._penbl.data))
                         time.sleep(self.SPEEDTIMES[self.turtlespeed])
-                        if self.showturtle: self._clearTurtle(blockBetween.x, blockBetween.y-2, blockBetween.z)
+                        if self.showturtle: self._clearTurtle(blBetween.x, blBetween.y-2, blBetween.z)
                         #self.mc.postToChat(-self.verticalheading)
                     #update turtle's position to be the target
                     self.position.x, self.position.y, self.position.z = x,y,z
                     #draw turtle
                     if self.showturtle: self._drawTurtle(targetX, targetY - 2, targetZ)
                 else:
-                    for blockBetween in blocksBetween:
-                        #print blockBetween
+                    for blBetween in blsBetween:
+                        #print blBetween
                         #if walking update the y, to be the height of the world
-                        if self.flying == False: blockBetween.y = self.mc.getHeight(blockBetween.x, blockBetween.z)
+                        if self.flying == False: blBetween.y = int(self.conn.sendReceive("world.getHeight", intFloor(blBetween.x, blBetween.z)))
                         #draw the turtle
-                        if self.showturtle: self._drawTurtle(blockBetween.x, blockBetween.y, blockBetween.z)
-                        if self._pendown: self.mcDrawing.drawPoint3d(blockBetween.x, blockBetween.y - 1, blockBetween.z, self._penblock.id, self._penblock.data)
+                        if self.showturtle: self._drawTurtle(blBetween.x, blBetween.y, blBetween.z)
+                        if self._pendown: self.conn.send("world.setBlock", intFloor(blBetween.x, blBetween.y - 1, blBetween.z, self._penbl.id, self._penbl.data))
                         time.sleep(self.SPEEDTIMES[self.turtlespeed])
-                        if self.showturtle: self._clearTurtle(blockBetween.x, blockBetween.y, blockBetween.z)
+                        if self.showturtle: self._clearTurtle(blBetween.x, blBetween.y, blBetween.z)
                         #self.mc.postToChat(self.verticalheading)
                     #update turtle's position to be the target
                     self.position.x, self.position.y, self.position.z = x,y,z
@@ -198,7 +186,8 @@ class MinecraftTurtle:
 
     def setposition(self, x, y, z, absolute=False):
         if not absolute:
-            pos = self.mc.entity.getTilePos(self.player)
+            s = self.conn.sendReceive("entity" + ".getTile", self.target)
+            pos = Vec3(*map(int, s.split(",")))
             #clear the turtle
             if self.showturtle:
                 self._clearTurtle(self.position.x,
@@ -258,20 +247,20 @@ class MinecraftTurtle:
         self.flying = False
         self.verticalheading = 0
 
-    def penblock(self, blockId, blockData = 0):
-        self._penblock = block.Block(blockId, blockData)
+    def penblock(self, blId, blData = 0):
+        self._penbl = bl.Block(blId, blData)
 
     def speed(self, turtlespeed):
         self.turtlespeed = turtlespeed
 
     def _drawTurtle(self,x,y,z):
         #draw turtle
-        self.mcDrawing.drawPoint3d(x, y, z, self.turtleblock.id, self.turtleblock.data)
-        self.lastDrawnTurtle = minecraft.Vec3(x,y,z)
+        self.conn.send("world.setBlock", intFloor(x, y, z, self.turtlebl.id, self.turtlebl.data))
+        self.lastDrawnTurtle = Vec3(x,y,z)
 
     def _clearTurtle(self,x,y,z):
         #clear turtle
-        self.mcDrawing.drawPoint3d(x, y, z, block.AIR.id)
+        self.conn.send("world.setBlock", intFloor(x, y, z, bl.AIR.id))
 
     def _findTargetBlock(self, turtleX, turtleY, turtleZ, heading, verticalheading, distance):
         x,y,z = self._findPointOnSphere(turtleX, turtleY, turtleZ, heading, verticalheading, distance)
@@ -286,10 +275,10 @@ class MinecraftTurtle:
         z = cz + (radius * (math.cos(math.radians(verticalAngle)) * math.sin(math.radians(horizontalAngle))))
         return x, y, z
 
-    def _roundXYZ(x,y,z):
+    def _roundXYZ(self, x,y,z):
         return int(round(x,0)), int(round(y,0)), int(round(z,0))
 
-    def _roundVec3(position):
-        return minecraft.vec3(int(position.x), int(position.y), int(position.z))
+    def _roundVec3(self, position):
+        return Vec3(int(position.x), int(position.y), int(position.z))
 
 
