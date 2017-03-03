d8a61eca8f7444be166a0165533ce90587a69194
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-03-03 14:41:28 +0100

    first commit of this branch

--- projects/test.py
+++ projects/test.py
@@ -3,51 +3,51 @@ from stuff.pycraft import *
 pos = where()
 chat(pos)
 
-# maze("maze1.csv")
-#
-# t = turtle(ice)
-# t.forward(10)
-#
-# move(3, 10, 5)
-#
-# chat(where())
-#
-# sphere(ice)
-#
-# circle([wool, 5], direction="horizontal")
-#
-# line(gold, 0, 0, 0, 0, 50, 0)
-#
-# block(ice, y=3)
-#
-# blocks(ice, x=5, y=6, z=10)
-#
-# size = readnumber("tell the size...")
-#
-# cube(redstone, size)
-#
-# text = readstring("say something...")
-#
-# chat("I said: " + text)
-#
-# pyramid(sandstone)
-#
-# polygon(obsidian, 12, 30)
-#
-# chat("Hello Minecraft!")
-#
-# color = 0
-#
-# uga = turtle([wool, color])
-#
-# while True:
-#
-#     for i in range(18):
-#         uga.forward(5)
-#         uga.up(20)
-#     uga.up(30)
-#     color += 1
-#     uga.penblock([wool, color % 12])
+maze("maze1.csv")
+
+t = turtle(ice)
+t.forward(10)
+
+move(3, 10, 5)
+
+chat(where())
+
+sphere(ice)
+
+circle([wool, 5], direction="horizontal")
+
+line(gold, 0, 0, 0, 0, 50, 0)
+
+block(ice, y=3)
+
+blocks(ice, x=5, y=6, z=10)
+
+size = readnumber("tell the size...")
+
+cube(redstone, size)
+
+text = readstring("say something...")
+
+chat("I said: " + text)
+
+pyramid(sandstone)
+
+polygon(obsidian, 12, 30)
+
+chat("Hello Minecraft!")
+
+color = 0
+
+uga = turtle([wool, color])
+
+while True:
+
+    for i in range(18):
+        uga.forward(5)
+        uga.up(20)
+    uga.up(30)
+    color += 1
+    uga.penblock([wool, color % 12])
 
 
 # GOLD in ICE
