21e575b798e54bfd22be1e4d7265910f7e807ed6
Alessandro Norfo <sprintingkiwi@gmail.com>
2017-06-28 09:27:06 +0200

    .

--- projects/stuffed_sphere.py
+++ projects/stuffed_sphere.py
@@ -1,4 +1,7 @@
 from stuff.pycraft import *
 
+# Create a sphere of glass (so you can see through)
 sphere(glass, 20, z=25)
+# Now create a sphere of lava (but also water or sand could work)
+# that will be generated inside the glass sphere
 sphere(lava, 19, z=25)
--- projects/turtle_square_spiral.py
+++ projects/turtle_square_spiral.py
@@ -1,10 +1,15 @@
 from stuff.pycraft import *
 
-uga = turtle(obsidian)
+# Instance a Turtle object
+uga = Turtle(obsidian)
 
+# Create a variable for the side of the spiral
 side = 1
 
 while True:
+    # Move the Turtle forward of "side" steps
     uga.forward(side)
+    # Make the Turtle turn right of 90 (try to change it) degrees
     uga.right(90)
+    # Increment the side variable
     side = side + 1
