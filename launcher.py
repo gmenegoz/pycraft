d5b835378f276f3580107f2028c98af7980dd5b0
Giuseppe Menegoz <gmenegoz@users.noreply.github.com>
2018-02-28 22:28:23 +0100

    Revert "Issue #7"

--- launcher.py
+++ launcher.py
@@ -1,40 +1,6 @@
-import os
-import urllib
 import subprocess
-import sys
 
-#print('By continuing you are indicating your agreement to our EULA https://account.mojang.com/documents/minecraft_eula')
-#raw_input('Press ENTER to continue...')
+print("Start")
 
 # Launch spigot server
-
-mswinplatform = sys.platform == "win32"
-
-if mswinplatform:
-    subprocess.Popen(['java', '-jar', 'spigot-1.10.2.jar'], creationflags=subprocess.CREATE_NEW_CONSOLE, cwd='server/')
-else:
-    subprocess.Popen(" ".join(['java', '-jar', 'spigot-1.10.2.jar']), shell=True, cwd='server/')
-    
-
-
-subprocess.Popen(['python', 'scratch_pycraft.py'], cwd='projects/pycraft/')
-
-# Check for scratch_pycraft file
-#os.chdir('projects/pycraft/')
-#filenames = os.listdir('.')
-#check = False
-#for f in filenames:
-    #if f == 'scatch_pycraft.py':
-        #check = True
-        #print('scratch_pycraft.py found, executing it...')
-        #subprocess.Popen(['python', 'scratch_pycraft.py'], shell=True)
-#if not check:
-    #try:
-        #urllib.urlretrieve('https://raw.githubusercontent.com/sprintingkiwi/pycraft_mod/development/scratch_pycraft.py',
-                           #'scratch_pycraft.py')
-    #except:
-        #print('Cannot retrieve scratch_pycraft.py')
-    #else:
-        #print('scratch_pycraft.py successfully retrieved, executing it...')
-        #subprocess.Popen(['python', 'scratch_pycraft.py'], shell=True)
-
+p = subprocess.Popen(['java', '-jar', 'spigot-1.10.2.jar'], cwd='server/')
